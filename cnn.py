df14792f6cc320898ada75a17db692699f23414e
David Clark <dmclark1729@gmail.com>
2018-04-01 14:02:37 -0500

    added dropout.py

--- cnn.py
+++ cnn.py
@@ -17,8 +17,8 @@ class ThreeLayerConvNet(object):
   """
   
   def __init__(self, input_dim=(3, 32, 32), num_filters=[32,32], filter_size=[7,7],
-               hidden_dim=[256,100], num_classes=10, weight_scale=1e-3, reg=0.0,
-               dtype=np.float32):
+               hidden_dim=[512,128], num_classes=10, weight_scale=1e-3, reg=0.0,
+               dtype=np.float32, dropout=0.0,seed=None):
     """
     Initialize a new network.
     
@@ -39,6 +39,7 @@ class ThreeLayerConvNet(object):
     self.num_filters = num_filters
     self.hidden_dim = hidden_dim
     self.filter_size = filter_size
+    self.use_dropout = dropout > 0
     
     stride = 1
     pad = (filter_size[0] - 1) / 2
@@ -64,7 +65,14 @@ class ThreeLayerConvNet(object):
       self.params['theta'+str(i+1)] = np.reshape(np.random.normal(0, weight_scale, dims[i-1]*dims[i]),[dims[i-1], dims[i]])
       
       self.params['theta'+str(i+1)+'_0'] = np.zeros(shape=[dims[i]])
-    
+
+
+    self.dropout_param = {}
+    if self.use_dropout:
+      self.dropout_param = {'mode': 'train', 'p': dropout}
+      if seed is not None:
+        self.dropout_param['seed'] = seed
+   
     for k, v in self.params.iteritems():
       print k, np.shape(v)
 
@@ -81,7 +89,13 @@ class ThreeLayerConvNet(object):
     conv - relu - 2x2 max pool - affine - relu - affine - softmax
     
     """
-    
+    mode = 'test' if y is None else 'train'
+
+    # Set train/test mode for  dropout param since they
+    # behave differently during training and testing.
+    if self.dropout_param is not None:
+      self.dropout_param['mode'] = mode   
+   
     # pass conv_param to the forward pass for the convolutional layer
     filter_size = self.filter_size[0]
     conv_param = {'stride': 1, 'pad': (filter_size - 1) / 2}
@@ -97,7 +111,10 @@ class ThreeLayerConvNet(object):
     ############################################################################
     # about 3 lines of code (use the helper functions in layer_utils.py)
     cache_dict = {}
+    drop_cache_dict = {}
     out = X
+
+    # adds the conv_relu layers
     for i in range(0, len(self.num_filters)-1):
       out, cache = conv_relu_forward(out,
                                      self.params['theta'+str(i+1)],
@@ -105,17 +122,22 @@ class ThreeLayerConvNet(object):
                                      conv_param)
       cache_dict['theta'+str(i+1)] = cache
     
+    # The last conv layer has a max pool 
     out, cache = conv_relu_pool_forward(out,
                                         self.params['theta'+str(len(self.num_filters))],
                                         self.params['theta'+str(len(self.num_filters))+'_0'],
                                         conv_param, pool_param)
     cache_dict['theta'+str(len(self.num_filters))] = cache
     
+    #Adds the fully connected layers
     for i in range(len(self.num_filters), len(self.num_filters)+len(self.hidden_dim)):
       out, cache = affine_relu_forward(out,
                                        self.params['theta'+str(i+1)],
                                        self.params['theta'+str(i+1)+'_0'])
       cache_dict['theta'+str(i+1)] = cache
+      if self.use_dropout:
+        out, drop_cache = dropout_forward(out, self.dropout_param)
+        drop_cache_dict['theta'+str(i+1)] = drop_cache
 
     scores, cache = affine_forward(out,
                                    self.params['theta'+str(1+len(self.num_filters)+len(self.hidden_dim))],
@@ -145,9 +167,11 @@ class ThreeLayerConvNet(object):
                                                                                 cache_dict['theta'+str(i+1)])
 
     for i in range(len(self.num_filters)+len(self.hidden_dim)-1, len(self.num_filters)-1, -1):
+      if self.use_dropout:
+        dx = dropout_backward(dx, drop_cache_dict['theta'+str(i+1)])
+
       dx, grads['theta'+str(i+1)], grads['theta'+str(i+1)+'_0'] = affine_relu_backward(dx,
                                                                                        cache_dict['theta'+str(i+1)])
-
     i = len(self.num_filters)
     dx, grads['theta'+str(i)], grads['theta'+str(i)+'_0'] = conv_relu_pool_backward(dx,
                                                                                cache_dict['theta'+str(i)])
